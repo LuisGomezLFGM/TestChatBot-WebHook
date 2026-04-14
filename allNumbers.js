96f669035849cc2dd3d1b17691792b60176a1524
LuisGomezLFGM <163154359+LuisGomezLFGM@users.noreply.github.com>
2026-04-14 18:59:58 -0500

    Update allNumbers.js

--- allNumbers.js
+++ allNumbers.js
@@ -1,4 +1,4 @@
-await axios.post("https://luisfgomezm.app.n8n.cloud/webhook/a7c161f0-9318-40bc-8d32-4af9a9349cbd/webhook", {
+await axios.post("https://luisfgomezm.app.n8n.cloud/webhook-test/whatsApp", {
   from: req.body.entry[0].changes[0].value.messages[0].from,
   message: req.body.entry[0].changes[0].value.messages[0].text.body
 });
