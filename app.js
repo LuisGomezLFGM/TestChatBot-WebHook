a34db32705f8bb6874974e9d31f24e06973dd22e
LuisGomezLFGM <163154359+LuisGomezLFGM@users.noreply.github.com>
2026-04-14 19:01:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -35,3 +35,8 @@ app.post('/', (req, res) => {
 app.listen(port, () => {
   console.log(`\nListening on port ${port}\n`);
 });
+
+await axios.post("https://luisfgomezm.app.n8n.cloud/webhook-test/whatsApp", {
+  from: req.body.entry[0].changes[0].value.messages[0].from,
+  message: req.body.entry[0].changes[0].value.messages[0].text.body
+});
